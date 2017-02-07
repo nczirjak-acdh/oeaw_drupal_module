0cb04e212606cea6f30eee921345e5749a153333
nczirjak-acdh <norbert.czirjak@oeaw.ac.at>
2017-02-07 11:31:09 +0100

    Datatable changes

--- js/datatable_init.js
+++ js/datatable_init.js
@@ -1,5 +1,8 @@
 var jq2 = jQuery;
 jQuery.noConflict(true);
 jq2(function( $ ) {
-       jq2('table.display').DataTable();
+       jq2('table.display').DataTable({
+            "pageLength": 25
+        });
+       
 });
